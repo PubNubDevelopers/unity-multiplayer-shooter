ce83007883d3a3bb80b52d5003a9276b977722ed
olivercarson-pubnub <oliver.carson@pubnub.com>
2023-02-22 21:24:12 -0800

    Adding CachedPlayers

--- Assets/SuperMultiplayerShooter/Scripts/PubNubManager.cs
+++ Assets/SuperMultiplayerShooter/Scripts/PubNubManager.cs
@@ -9,6 +9,9 @@ public class PubNubManager : MonoBehaviour
     //Persist the PubNub object across scenes
     private static string _userID;
 
+    //Cached players from connection.
+    private static Dictionary<string, PNUUIDMetadataResult> _cachedPlayers = new Dictionary<string, PNUUIDMetadataResult>();
+
     public static PubNub PubNub;
 
     public static string PublishKey
@@ -35,5 +38,11 @@ public class PubNubManager : MonoBehaviour
         }
         PubNubConnection = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public static Dictionary<string, PNUUIDMetadataResult> CachedPlayers
+    {
+        get { return _cachedPlayers; }
+        set { _cachedPlayers = value; }
     }
 }
