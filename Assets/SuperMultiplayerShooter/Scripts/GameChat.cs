ef8e63eac28b817468cefc4a163ddee7774052dd
olivercarson-pubnub <oliver.carson@pubnub.com>
2023-03-25 10:06:41 -0700

    Update with profanity handling in game chat.
    Implementing publishing and receiving profanity in chat.

--- Assets/SuperMultiplayerShooter/Scripts/GameChat.cs
+++ Assets/SuperMultiplayerShooter/Scripts/GameChat.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MiniJSON;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Photon.Pun;
 using PubNubAPI;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,8 +24,8 @@ public class GameChat : MonoBehaviour
     public InputField inputField;
 
     //Internals
-    private string _gameSubscribe = "chat.ingame"; //Wildcard subscribe to listen for all channels
-    private string _gamePublish = "chat.ingame."; //Publish channel will include
+    private string _gameSubscribe = "chat.game."; //Wildcard subscribe to listen for all channels
+    private string _gamePublish = "chat.game."; //Publish channel will include
     private PubNub _pubnub;
 
     // Start is called before the first frame update
@@ -39,7 +43,8 @@ public class GameChat : MonoBehaviour
             SubscribeEventEventArgs mea = e as SubscribeEventEventArgs;
             if (mea.MessageResult != null)
             {
-                GetUsername(mea.MessageResult);
+                string message = ParseMessage(mea.MessageResult.Payload);
+                GetUsername(mea.MessageResult, message);
             }
         };
 
@@ -62,6 +67,9 @@ public class GameChat : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called when the scene ends or the game closes. Unsubscribe from game channel.
+    /// </summary>
     private void OnDestroy()
     {
         //Unsubscrube from lobby chat once player leaves the room.
@@ -90,12 +98,12 @@ public class GameChat : MonoBehaviour
     {
         if (!string.IsNullOrEmpty(inputField.text))
         {
-            //MessageModeration filter = new MessageModeration();
-            //filter.text = inputField.text;
+            MessageModeration filter = new MessageModeration();
+            filter.text = inputField.text;
 
             _pubnub.Publish()
                 .Channel(_gamePublish)
-                .Message(inputField.text)
+                .Message(filter)
                 .Async((result, status) => {
                     if (status.Error)
                     {
@@ -109,7 +117,7 @@ public class GameChat : MonoBehaviour
     }
 
     /// <summary>
-    /// Displays the chat mesage
+    /// Displays the chat mesage. If the message contains profanity, simply prints the whole message as asterisks (*)
     /// </summary>
     /// <param name="message">The message from the user</param>
     /// <param name="from">The user who the sent the message</param>
@@ -138,21 +146,42 @@ public class GameChat : MonoBehaviour
     /// Obtains the username and displays the chat.
     /// </summary>
     /// <param name="message"></param>
-    private void GetUsername(PNMessageResult message)
+    private void GetUsername(PNMessageResult payload, string message)
     {
         string username = "";
-        if (PubNubManager.Instance.CachedPlayers.ContainsKey(message.IssuingClientId))
+        if (PubNubManager.Instance.CachedPlayers.ContainsKey(payload.IssuingClientId))
         {
-            username = PubNubManager.Instance.CachedPlayers[message.IssuingClientId].Name;
+            username = PubNubManager.Instance.CachedPlayers[payload.IssuingClientId].Name;
         }
 
         //Check in case the username is null. Set to back-up of UUID just in case.
         if (string.IsNullOrWhiteSpace(username))
         {
-            username = message.IssuingClientId;
+            username = payload.IssuingClientId;
         }
 
         //Display the chat pulled.
-        DisplayChat(message.Payload.ToString(), username, PubNubManager.Instance.UserId.Equals(message.IssuingClientId), false, false);
+        DisplayChat(message, username, PubNubManager.Instance.UserId.Equals(payload.IssuingClientId), false, false);
+    }
+
+    /// <summary>
+    /// Parses the payload for messages containing containing both non-profanity/profanity messages.
+    /// If the payload does contain profanity, simply prints the entire message as asterisks.
+    /// You can, however, isolate the profanity and replace only the words containing profanity with asterisks.
+    /// </summary>
+    /// <param name="payload"></param>
+    /// <returns></returns>
+    private string ParseMessage(object payload)
+    {
+        object message = "";
+        var metaDataJSON = JsonConvert.SerializeObject(payload);
+
+        //Extracting text from the payload. If it cannot be found, simply print a temp message.
+        var messageDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(metaDataJSON);
+        if (messageDictionary == null || !messageDictionary.TryGetValue("text", out message))
+        {
+            message = "<...>";
+        }
+        return message.ToString();
     }
 }
